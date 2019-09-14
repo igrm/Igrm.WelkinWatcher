e823887c3e578f206345325ac3703b3400775e1b
igrm <igor.mishur@gmail.com>
2019-09-14 16:41:37 +0300

    Initial commit

---
+++ Igrm.WelkinWatcher.BackgroundWorker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Igrm.WelkinWatcher.BackgroundWorker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
