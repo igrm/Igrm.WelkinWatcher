56d13d0975019bc83635add9ab5e6378cb0cdb03
igrm <igor.mishur@gmail.com>
2019-09-16 09:54:20 +0300

    Initial Host

--- Igrm.WelkinWatcher.BackgroundWorker/Program.cs
+++ Igrm.WelkinWatcher.BackgroundWorker/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
 namespace Igrm.WelkinWatcher.BackgroundWorker
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var host = new HostBuilder()
+                          .UseConsoleLifetime()
+                          .Build();
+
+            await host.RunAsync();
         }
     }
 }
