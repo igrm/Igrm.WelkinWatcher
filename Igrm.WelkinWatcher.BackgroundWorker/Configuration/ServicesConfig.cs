31e6310f21dd048303364cafa4b0b5787893bbbc
igrm <igor.mishur@gmail.com>
2019-09-17 15:09:24 +0300

    Rebus and RabbitMQ added

--- Igrm.WelkinWatcher.BackgroundWorker/Configuration/ServicesConfig.cs
+++ Igrm.WelkinWatcher.BackgroundWorker/Configuration/ServicesConfig.cs
@@ -8,6 +8,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Igrm.WelkinWatcher.BackgroundWorker.Services;
+using Rebus.ServiceProvider;
+using Rebus.Config;
+using RabbitMQ.Client;
 
 namespace Igrm.WelkinWatcher.BackgroundWorker.Configuration
 {
@@ -16,9 +19,15 @@ namespace Igrm.WelkinWatcher.BackgroundWorker.Configuration
         public static Action<HostBuilderContext,IServiceCollection> Execute =>
               (context, collection) =>
               {
+                  var connectionFactory = new ConnectionFactory();
+                  context.Configuration.GetSection("RabbitMqConnection").Bind(connectionFactory);
+
                   collection.AddLogging();
                   collection.AddHttpClient();
+                  collection.AddRebus(configure => configure.Logging(l => l.Serilog())
+                                                            .Transport(t=>t.UseRabbitMq(connectionFactory.Uri.ToString(),"default")));
                   collection.AddTransient<IHostedService, StateVectorsHostedService>();
+                  
               };
     }
 }
