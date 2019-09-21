d1a4815c1768c9f3b8ade4289094640790b8d42e
igrm <igor.mishur@gmail.com>
2019-09-21 15:20:42 +0300

    infoboard implementation

--- Igrm.WelkinWatcher.InfoBoard/wwwroot/js/site.js
+++ Igrm.WelkinWatcher.InfoBoard/wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿const connection = new signalR.HubConnectionBuilder()
+﻿var headerAdded = false;
+
+const connection = new signalR.HubConnectionBuilder()
     .withUrl("ws://localhost:64323/stateVectors", {
         skipNegotiation: true,
         transport: signalR.HttpTransportType.WebSockets
@@ -15,6 +17,44 @@ connection.on("ReceiveStateVector", (stateVector) => {
 });
 
 function ReceiveStateVector(stateVector) {
-    console.log(stateVector);
+
+    if (!headerAdded) {
+        var head = $("#template").clone();
+        var headCounter = 1;
+        $.each(stateVector, function (propertyName, valueOfProperty) {
+            var element = head.find("td:nth-child(" + headCounter + ")");
+            element.text(propertyName);
+            headCounter++;
+        });
+        $("#vectors").append("<tr id='head'>" + head.html() + "</tr>");
+        headerAdded = true;
+    }
+
+    var copy = $("#template").clone();
+    var counter = 1;
+
+    $.each(stateVector, function (propertyName, valueOfProperty) {
+        var element = copy.find("td:nth-child(" + counter + ")");
+
+        if ($("#" + stateVector.icao24 + "-" + propertyName).length > 0 && $("#" + stateVector.icao24 + "-" + propertyName).text() != valueOfProperty) {
+            element.addClass("blink_me");
+        }
+        else {
+            $("#" + stateVector.icao24 + "-" + propertyName).removeClass("blink_me");
+            element.removeClass("blink_me");
+        }
+
+        element.text(valueOfProperty);
+        element.attr("id", stateVector.icao24 + "-" + propertyName);
+        
+        counter++;
+    });
+
+    if ($("#" + stateVector.icao24).length === 0) {
+        $("#vectors").append("<tr id='" + stateVector.icao24 + "'>" + copy.html() + "</tr>");
+    }
+    else {
+        $("#" + stateVector.icao24).html(copy.html());
+    }
 }
 
